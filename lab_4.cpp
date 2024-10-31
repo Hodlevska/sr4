62cefc9ec5e3f42805bf15c50f4f65d3889ac395
Hodlevska <73657261+Hodlevska@users.noreply.github.com>
2024-10-31 20:49:30 +0000

    First commut

--- lab_4.cpp
+++ lab_4.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+ int main() 
+ {
+    setlocale(0, ".1251");
+    double y, x;     
+    cout << "Введіть значення х\n"; 
+    return 0;
+} 
+ 
 
